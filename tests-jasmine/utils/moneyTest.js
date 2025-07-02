42f07455ada16d7434df1c3584d7528f22414a28
Dpsingh <dpsingh52282@gmail.com>
2025-07-02 22:50:16 +0530

    automated all tests and chaged all files accordingly

--- tests-jasmine/utils/moneyTest.js
+++
@@ -1,7 +0,0 @@
-import {formatCurrency } from '../../scripts/utils/money.js';
-
-describe('test suite: formatCurrency', ()=>{
-    it('converts cents into dollars', ()=>{
-        expect(formatCurrency(2000.5)).toEqual('20.01');
-        });
-});
